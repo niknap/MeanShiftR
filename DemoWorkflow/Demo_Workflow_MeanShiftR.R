db0b00b39559274ae826370a42aef66938f2e40b
Nikolai Knapp knappn <knappn>
2018-09-02 13:20:20 +0200

    Update Demo Workflow

--- DemoWorkflow/Demo_Workflow_MeanShiftR.R
+++ DemoWorkflow/Demo_Workflow_MeanShiftR.R
@@ -3,82 +3,119 @@
 # adaptive mean shift 3D algorithm (AMS3D) implemented in MeanShiftR package
 ############################################################################################
 
+#############
+# Preparation
+#############
+
 # Load required packages
 .libPaths()
 require(MeanShiftR)
 
-# Set working directory
-# wd <- "C:\\Users\\knappn\\owncloud\\Laptop\\RScripts\\PackageDevelopment_MeanShiftR\\TestFiles\\"
-# setwd(wd)
-
 # Read and inspect example data
-lid.dt <- readRDS("Traunstein.rectangle.lidar.dt.rds")
-display.point.cloud.dt(lid.dt)
-nrow(lid.dt)/180000
+require(repmis)
+source_data("https://github.com/niknap/MeanShiftR/blob/master/DemoWorkflow/Demo_Data_1ha_Traunstein.rda?raw=True")
+head(lid.dt)
+head(inv.dt)
+nrow(lid.dt)
+nrow(inv.dt)
 
-# Subset the data for testing
-lid.dt <- subset(lid.dt, X < 400 & Y < 200)
-display.point.cloud.dt(lid.dt, size=2)
+# Display the point cloud (requires function from slidaRtools package available here: https://github.com/niknap/slidaRtools;
+# alternatively use the rgl::plot3d function to write your custom 3D plotting code)
+require(slidaRtools)
+slidaRtools::display.point.cloud.dt(lid.dt, size=2)
 
-# Split the point cloud into 50-m tiles with 10-m buffers around
-lid.list <- split_BufferedPointCloud(pc.dt=lid.dt, plot.width=100, buffer.width=10)
+####################################
+# Data splitting for parallelization
+####################################
+
+# Split the point cloud into 25-m tiles with 10-m buffers around each tile
+lid.list <- split_BufferedPointCloud(pc.dt=lid.dt, plot.width=25, buffer.width=10)
 length(lid.list)
 
 # Check the result by plotting one example tile color coded as core and buffer area
 require(slidaRtools)
-display.point.cloud.dt(lid.list[[10]], col.lim=c(0, 1), col.var="Buffer", size=3)
+slidaRtools::display.point.cloud.dt(lid.list[[6]], col.lim=c(0, 1), col.var="Buffer", size=3)
 
-# Apply the parallel AMS3D in the fast "voxel" version
-system.time(clus.dt <- parallel_MeanShift(pc.list=lid.list, lib.path=.libPaths()[1], frac.cores=0.5, version="voxel",
-                                          H2CW=0.3, H2CL=0.4, max.iter=20, buffer.width=10, minz=2, ctr.ac=2))
-260/18
-55/18
+################
+# Parallel AMS3D
+################
 
 # Apply the parallel AMS3D in the slower "classic" version
 system.time(clus.dt <- parallel_MeanShift(pc.list=lid.list, lib.path=.libPaths()[1], frac.cores=0.5, version="classic",
                                           H2CW=0.3, H2CL=0.4, max.iter=20, buffer.width=10, minz=2, ctr.ac=2))
-1200/18
+
+# Apply the parallel AMS3D in the fast "voxel" version
+system.time(clus.dt <- parallel_MeanShift(pc.list=lid.list, lib.path=.libPaths()[1], frac.cores=0.5, version="voxel",
+                                          H2CW=0.3, H2CL=0.4, max.iter=20, buffer.width=10, minz=2, ctr.ac=2))
+
+##################
+# Unparallel AMS3D
+##################
+
+# Apply the AMS3D in the fast "voxel" version on one large 100-m tile (no parallelization)
+lid.list <- split_BufferedPointCloud(pc.dt=lid.dt, plot.width=100, buffer.width=10)
+length(lid.list)
+system.time(clus.dt <- parallel_MeanShift(pc.list=lid.list, lib.path=.libPaths()[1], frac.cores=0.5, version="voxel",
+                                          H2CW=0.3, H2CL=0.4, max.iter=20, buffer.width=10, minz=2, ctr.ac=2))
+
+# Caution: The execution of the following code may take some time!!!
+# Apply the AMS3D in the slow "classic" version on one large 100-m tile (no parallelization)
+lid.list <- split_BufferedPointCloud(pc.dt=lid.dt, plot.width=100, buffer.width=10)
+length(lid.list)
+system.time(clus.dt <- parallel_MeanShift(pc.list=lid.list, lib.path=.libPaths()[1], frac.cores=0.5, version="classic",
+                                          H2CW=0.3, H2CL=0.4, max.iter=20, buffer.width=10, minz=2, ctr.ac=2))
+
+#######################
+# Check output visually
+#######################
 
 # Count how many tree crown clusters have been found
 (Nclus <- length(unique(clus.dt$ID)))
 
 # Display the clustered point cloud with each tree crown in a random color
-display.point.cloud.dt(clus.dt, col.var="ID", col.palette=rainbow(Nclus)[sample(Nclus)], col.lim=c(0, Nclus), size=3)
+slidaRtools::display.point.cloud.dt(clus.dt, col.var="ID", col.palette=rainbow(Nclus)[sample(Nclus)], col.lim=c(0, Nclus), size=3)
+
+###############################
+# Derive crown projection areas
+###############################
 
 # Fit polygons of different shapes for the crown projection areas
-circles.spdf <- make_CrownPolygons(pc.dt=clus.dt, type="circle")
 ellipses.spdf <- make_CrownPolygons(pc.dt=clus.dt, type="ellipse")
+circles.spdf <- make_CrownPolygons(pc.dt=clus.dt, type="circle")
 chulls.spdf <- make_CrownPolygons(pc.dt=clus.dt, type="convexhull")
 
-# Create and display a canopy height model together with the crown polygons
-chm.ras <- raster.from.point.cloud(lid.dt)
+# Create and display a canopy height model (CHM) together with the crown polygons
+# (alternative function for CHM generation can be found in the raster package)
+require(slidaRtools)
+chm.ras <- slidaRtools::raster.from.point.cloud(lid.dt)
 plot(chm.ras)
-plot(circles.spdf, add=T, border="red")
 plot(ellipses.spdf, add=T, border="blue")
+plot(circles.spdf, add=T, border="red")
 plot(chulls.spdf, add=T, border="black")
 
-# Read example inventory data
-inv.dt <- data.table(readRDS("Traunstein.rectangle.inventory.df.rds"))
+######################################
+# Match crowns with stems in inventory
+######################################
 
-# Subset the data for testing
-inv.dt <- subset(inv.dt, X < 100 & Y < 100)
-
-# Convert to SpatialPointsDataFrame
+# Convert inventory to SpatialPointsDataFrame
 inv.spdf <- SpatialPointsDataFrame(coords=cbind(X=inv.dt$X, Y=inv.dt$Y), data=inv.dt)
 
 # Plot inventory data
-plot(inv.spdf, cex=inv.spdf$DBH, pch=16, col="brown", add=T)
+plot(inv.spdf, cex=inv.spdf@data$DBH, pch=16, col="brown", add=T)
+
+# Add Species column which is required for the following function
+inv.spdf@data$Species <- NA
 
 # Find a stem for each crown polygon
 ellipses.spdf <- match_CrownsStems(crowns.spdf=ellipses.spdf, stems.spdf=inv.spdf, DBH.min=0.05)
 head(ellipses.spdf)
 
 # Plot tree height vs. DBH
-plot(ellipses.spdf$TreeH ~ ellipses.spdf$DBH)
+plot(ellipses.spdf@data$TreeH ~ ellipses.spdf@data$DBH, xlab="DBH (m)", ylab="Tree height (m)")
 
 # Plot mean tree crown diameter vs. DBH
-ellipses.spdf$MeanCrownDiameter <- 2 * ellipses.spdf$MeanRadius
-plot(ellipses.spdf$MeanCrownDiameter ~ ellipses.spdf$DBH)
+ellipses.spdf@data$MeanCrownDiameter <- 2 * ellipses.spdf@data$MeanRadius
+plot(ellipses.spdf@data$MeanCrownDiameter ~ ellipses.spdf@data$DBH, xlab="DBH (m)", ylab="Crown diameter (m)")
 
 
 
